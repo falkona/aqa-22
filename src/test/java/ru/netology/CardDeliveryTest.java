b9c5ce91f69d5e640e1e8605b24bddbd19fdace6
Dasha Nos <dasha.nos91@gmail.com>
2019-11-02 16:43:15 +0300

    shouldBeUnavailableDeliveryIfPhoneIsTooLong added

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -61,6 +61,48 @@ public class CardDeliveryTest {
         $("[data-test-id=city] .input__sub").shouldHave(Condition.exactText("Доставка в выбранный город недоступна"));
     }
 
+    @Test
+    void shouldBeUnavailableDeliveryIfNameHasWrongSymbols (){
+        open(getBaseUrl());
+
+        $("[data-test-id=city] input").setValue("Воронеж");
+        sleepSugar(2);
+        $("[data-test-id=date] input").clear();
+        $("[data-test-id=date] input").setValue("07.11.2019");
+        sleepSugar(2);
+        $("[data-test-id=name] input").setValue("Дарья 2");
+        sleepSugar(2);
+        $("[data-test-id=phone] input").setValue("+79032596295");
+        sleepSugar(2);
+        $("[data-test-id=agreement]").click();
+        sleepSugar(2);
+        $$("button").find(Condition.exactText("Забронировать")).click();
+        sleepSugar(2);
+        $("[data-test-id=name] .input__sub").shouldBe(Condition.visible);
+        $("[data-test-id=name] .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldBeUnavailableDeliveryIfPhoneIsTooLong (){
+        open(getBaseUrl());
+
+        $("[data-test-id=city] input").setValue("Санкт-Петербург");
+        sleepSugar(2);
+        $("[data-test-id=date] input").clear();
+        $("[data-test-id=date] input").setValue("10.11.2019");
+        sleepSugar(2);
+        $("[data-test-id=name] input").setValue("Дарья");
+        sleepSugar(2);
+        $("[data-test-id=phone] input").setValue("+790325962951");
+        sleepSugar(2);
+        $("[data-test-id=agreement]").click();
+        sleepSugar(2);
+        $$("button").find(Condition.exactText("Забронировать")).click();
+        sleepSugar(2);
+        $("[data-test-id=phone] .input__sub").shouldBe(Condition.visible);
+        $("[data-test-id=phone] .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
     public String getBaseUrl() {
         return baseUrl;
     }
